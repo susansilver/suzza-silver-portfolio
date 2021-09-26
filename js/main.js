de18781c8a0641bf5432b1aea8e8112f0142f479
Susan Silver <susansilver@users.noreply.github.com>
2021-09-26 12:22:26 -0700

    Add js files

--- js/main.js
+++ js/main.js
@@ -1,1 +1,33 @@
+// Sticky Nav
+// http://www.mattmorgante.com/technology/sticky-navigation-bar-javascript
+const nav = document.querySelector('#navigation');
+const navTop = nav.offsetTop;
 
+function stickyNavigation() {
+  console.log('navTop = ' + navTop);
+  console.log('scrollY = ' + window.scrollY);
+
+  if (window.scrollY >= navTop) {
+    // nav offsetHeight = height of nav
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove('fixed-nav');
+  }
+}
+
+window.addEventListener('scroll', stickyNavigation);
+
+ //Add Active Class to Nav
+  //https://codepen.io/arjunamgain/pen/lGALt
+ 
+ /*  $(document).ready(function(){
+    $('ul li a').click(function(){
+      $('li a').removeClass("active");
+      $(this).addClass("active");
+  });
+  });  */ 
+
+
+ 
